28e33296a427df771f08e7ff2020f489ef677f4a
willkrakow <willkrakow@gmail.com>
2021-10-04 10:45:07 -0400

    Remove unused code. Refactor TaskList.

---
+++ client/src/components/TaskList/TaskListItem.tsx
@@ -0,0 +1,82 @@
+import React from 'react'
+import { ListItem, ListItemText, IconButton, ListItemButton, ListItemIcon, Checkbox } from '@mui/material'
+import { EditOutlined, DeleteOutline } from '@mui/icons-material'
+import { Task } from '../../types'
+
+interface Props {
+    task: Task
+    handleEditTask: (task: Task) => void
+    handleDeleteTask: (task: Task) => void
+    handleCompleteTask: (task: Task) => void
+}
+
+const TaskListItem = ({
+  task,
+  handleEditTask,
+  handleDeleteTask,
+  handleCompleteTask,
+}: Props): JSX.Element => {
+  return (
+    <ListItem
+      sx={{
+        borderWidth: "0.5px",
+        borderColor: "grey.100",
+        borderStyle: "solid",
+        borderRadius: "5px",
+        pt: 3,
+        pb: 2.5,
+        px: 0,
+        mb: 2.5,
+        backgroundColor: task.completed ? "info.100" : "inherit",
+      }}
+      disablePadding
+      secondaryAction={
+        <>
+          <IconButton size="small" onClick={() => handleEditTask(task)}>
+            <EditOutlined />
+          </IconButton>
+          <IconButton size="small" onClick={() => handleDeleteTask(task)}>
+            <DeleteOutline />
+          </IconButton>
+        </>
+      }
+    >
+      <ListItemButton
+        dense
+        role={undefined}
+        onClick={() => handleCompleteTask(task)}
+        sx={{ py: 0 }}
+      >
+        <ListItemIcon>
+          <Checkbox
+            color="secondary"
+            checked={task.completed}
+            edge="start"
+            tabIndex={-1}
+            disableRipple
+          />
+        </ListItemIcon>
+        <ListItemText
+          primary={task.title}
+          primaryTypographyProps={{
+            variant: "h3",
+            sx: {
+              textDecoration: task.completed ? "line-through" : "none",
+              color: task.completed ? "secondary.main" : "inherit",
+            },
+          }}
+          secondary={task.description}
+          secondaryTypographyProps={{
+            variant: "h4",
+            sx: {
+              textDecoration: task.completed ? "line-through" : "none",
+            },
+          }}
+        />
+      </ListItemButton>
+    </ListItem>
+  );
+}
+
+
+export default TaskListItem
--- client/src/components/TaskList/index.tsx
+++ client/src/components/TaskList/index.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { List, ListItem, ListItemButton, ListItemText, ListItemIcon, Checkbox, IconButton} from '@mui/material'
 import { EditOutlined, DeleteOutline } from '@mui/icons-material'
 import { Task } from '../../types'
+import TaskListItem from './TaskListItem'
 
 interface TaskListProps {
   tasks: Task[];
@@ -26,69 +27,12 @@ const TaskList = ({
 
   return (
     <List>
-      {tasks.map((task) => (
-        <ListItem
-          sx={{ 
-              borderWidth: "0.5px",
-              borderColor: "grey.100",
-              borderStyle: "solid",
-              borderRadius: "5px",
-              pt: 3,
-              pb: 2.5,
-              px: 0,
-              mb: 2.5,
-              backgroundColor: task.completed ? "info.100" : "inherit"
-           }}
-          disablePadding
-          key={task._id || Math.random()}
-          secondaryAction={
-            <>
-              <IconButton size="small" onClick={() => handleEditTask(task)}>
-                <EditOutlined />
-              </IconButton>
-              <IconButton size="small" onClick={() => handleDeleteTask(task)}>
-                <DeleteOutline />
-              </IconButton>
-            </>
-          }
-        >
-          <ListItemButton
-            dense
-            role={undefined}
-            onClick={() => handleCompleteTask(task)}
-            sx={{ py: 0 }}
-          >
-            <ListItemIcon>
-              <Checkbox
-                color="secondary"
-                checked={task.completed}
-                edge="start"
-                tabIndex={-1}
-                disableRipple
-              />
-            </ListItemIcon>
-            <ListItemText
-              primary={task.title}
-              primaryTypographyProps={{
-                variant: "h3",
-                sx: {
-                  textDecoration: task.completed ? "line-through" : "none",
-                  color: task.completed ? "secondary.main" : "inherit"
-                },
-              }}
-              secondary={task.description}
-              secondaryTypographyProps={{
-                variant: "h4",
-                sx: {
-                  textDecoration: task.completed ? "line-through" : "none",
-                },
-              }}
-            />
-          </ListItemButton>
-        </ListItem>
+      {tasks.map((task: Task) => (
+        <TaskListItem key={task?._id || Math.random()} task={task} handleEditTask={handleEditTask} handleDeleteTask={handleDeleteTask} handleCompleteTask={handleCompleteTask} />
       ))}
     </List>
   );
 }
 
 export default TaskList;
+
